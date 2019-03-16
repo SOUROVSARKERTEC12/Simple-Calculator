c5a11e1d50dcc501f41058a6adc69d59f517d7f4
SOUROVSARKAR <sourovsarker005@gmail.com>
2019-03-16 21:05:25 +0600

    Firt Commit

---
+++ Project_Calculator.java
@@ -0,0 +1,14 @@
+
+package project_calculator;
+
+public class Project_Calculator {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Simple_Calculator cf = new Simple_Calculator();
+        cf.setVisible(true);
+        cf.setTitle("Calculator");
+    }
+    
+}
